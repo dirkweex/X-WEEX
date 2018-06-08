bd38987b7a13d77592ee240aa84c570610d55693
unknown <weihao@zlgmcu.com>
2018-06-08 12:01:33 +0800

    完成新建dianzhan页面的布局。

---
+++ src/NewBuild.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="newbuild-bg">
+        <image :style="imageStyle" src="/web/assets/img/page1_bg.png" />
+        <div class="a">
+            <image class="a-img" src="/web/assets/img/head_1.png" />
+            <div class="a-a">
+                <text class="a-a-text">aw11sad2214asdasd12123</text>
+                <div class="a-a-a">
+                    <text class="a-a-a-text1">0 电站</text>
+                    <text class="a-a-a-text2">0 设备</text>
+                </div>
+            </div>
+        </div>
+
+        <div class="b">
+            <image class="b-img" src="/web/assets/img/search.png" />
+            <text class="b-text">咦？还没有自己的电站？<br><br> 立即新建一个吧！</text>
+            <div class="b-button">
+                <button class="b-button-text">新建电站</button>
+                <image class="b-button-img" src="/web/assets/img/add@2x.png" />
+
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            imageStyle: {
+                position: "absolute",
+                width: "750px",
+                height:
+                    weex.config.env.deviceHeight *
+                        750 /
+                        weex.config.env.deviceWidth +
+                    "px"
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.newbuild-bg {
+    flex: 1;
+}
+.a {
+    margin-top: 150px;
+    margin-left: 80px;
+    flex-direction: row;
+}
+.b {
+    justify-content: center;
+    align-items: center;
+    margin-top: 250px;
+}
+.a-img {
+    width: 100px;
+    height: 100px;
+}
+.a-a {
+    margin-left: 50px;
+}
+.a-a-text {
+    font-size: 30px;
+    color: #ffffff;
+}
+.a-a-a {
+    margin-top: 10px;
+    flex-direction: row;
+}
+.a-a-a-text1 {
+    font-size: 40px;
+    color: #ffffff;
+}
+.a-a-a-text2 {
+    font-size: 40px;
+    color: #ffffff;
+    margin-left: 100px;
+}
+.b-img {
+    width: 150px;
+    height: 150px;
+}
+.b-text {
+    margin-top: 45px;
+    color: #6f6f6f;
+    font-size: 35px;
+}
+.b-button {
+    flex-direction: row;
+    width: 400px;
+    margin-top: 50px;
+}
+.b-button-img {
+    position: absolute;
+    width: 35px;
+    margin-top: 23px;
+    margin-left: 45px;
+    height: 35px;
+}
+.b-button-text {
+    font-size: 35px;
+    width: 400px;
+    text-align: center;
+    padding: 15px;
+    border-width: 2px;
+    border-style: solid;
+    color: #ffffff;
+    border-color: #ffffff;
+    background-color: #2e9bf6;
+    border-radius: 50px;
+}
+</style>
