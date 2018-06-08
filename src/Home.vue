af9900b6386aaa93c6e74671ee2dd9b49b2009d6
unknown <weihao@zlgmcu.com>
2018-06-08 15:41:55 +0800

    完成主页收益曲线按钮布局。

--- src/Home.vue
+++ src/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="hemo-wrapper">
+    <!-- 我的电站 -->
     <div class="toobar">
       <div class="title">
         <text class="title-text">我的电站</text>
       </div>
       <image class="toobar-rightimage" src="/web/assets/img/add.png" />
     </div>
+    <!-- 今日发电和今日总收益 -->
     <div class="all-data">
       <div class="leftData">
         <text class="du-color">今日发电总量</text>
@@ -13,27 +15,49 @@
           <text class="du-text">3712.37</text>
           <text class="du-color">度</text>
         </div>
-           <div class="du-warpper">
-        <text class="du-color">累计总发电</text>
-        <text class="du-color2">3712.37</text>
-        <text class="du-color">度</text>
-           </div>
+        <div class="du-warpper">
+          <text class="du-color">累计总发电</text>
+          <text class="du-color2">3712.37</text>
+          <text class="du-color">度</text>
+        </div>
       </div>
       <div style="width:1px;height:120px;margin:30px;background-color:#9F9F9F" />
-          <div class="rightData">
+      <div class="rightData">
         <text class="du-color">今日总收益</text>
         <div class="du-warpper">
           <text class="du-text">2413.04</text>
           <text class="du-color"></text>
         </div>
-           <div class="du-warpper">
-        <text class="du-color">累计总收益</text>
-        <text class="du-color2">2413.04</text>
-        <text class="du-color"></text>
-           </div>
+        <div class="du-warpper">
+          <text class="du-color">累计总收益</text>
+          <text class="du-color2">2413.04</text>
+          <text class="du-color"></text>
+        </div>
       </div>
 
     </div>
+
+    <!-- 分割线 -->
+    <div style="height:20px;margin:30px;background-color:#EEEEEE" />
+
+    <!-- 收益曲线图之3个按钮 -->
+    <div class="button-warpper">
+      <button class="button">功率</button>
+      <button class="button">发电量</button>
+      <button class="button">收益</button>
+    </div>
+
+    <!-- 收益曲线图之曲线图 -->
+
+    <!-- 收益曲线图之年月日 -->
+    <div class="button-warpper2">
+      <button class="button2">日</button>
+      <button class="button3">月</button>
+      <button class="button3">年</button>
+      <button class="button3">总</button>
+      <image class="year-img" src="/web/assets/img/calendar.png" />
+    </div>
+
   </div>
 </template>
 <style scoped>
@@ -67,8 +91,8 @@
     align-items: center;
     margin-top: 30px;
 }
-.rightData{
-  margin-left: 20px;
+.rightData {
+    margin-left: 20px;
 }
 .du-warpper {
     flex-direction: row;
@@ -78,18 +102,74 @@
     color: #1e90ff;
     margin-right: 10px;
 }
-.du-color{
-  font-size: 25px;
-  color:#9F9F9F;
-  margin-top: 10px;
-  margin-bottom: 10px;
+.du-color {
+    font-size: 25px;
+    color: #9f9f9f;
+    margin-top: 10px;
+    margin-bottom: 10px;
 }
-.du-color2{
-  color:#555555;
-  font-size: 30px;
-  margin-left: 5px;
-  margin-right: 5px;
-  margin-top: 8px;
- 
+.du-color2 {
+    color: #555555;
+    font-size: 30px;
+    margin-left: 5px;
+    margin-right: 5px;
+    margin-top: 8px;
+}
+.button-warpper {
+    /* margin-top: 10px; */
+    flex-direction: row;
+    justify-content: space-around;
+}
+.button-warpper2{
+     flex-direction: row;
+    /* justify-content: space-around; */
+    margin-top: 30px;
+    
+}
+.button {
+    font-size: 30px;
+    width: 200px;
+    text-align: center;
+    padding: 15px;
+    border-width: 2px;
+    border-style: solid;
+    color: #ffffff;
+    border-color: #ffffff;
+    background-color: #3fa2f9;
+    border-radius: 50px;
+}
+.button2 {
+  margin-left: 30px;
+    font-size: 30px;
+    width: 90px;
+    height: 90px;
+    text-align: center;
+    border-width: 2px;
+    border-style: solid;
+    color: #ffffff;
+    border-color: #ffffff;
+    background-color:#31D7FE;
+    border-radius: 100px;
+
+}
+.button3 {
+  margin-left: 60px;
+    font-size: 30px;
+    width: 90px;
+    height: 90px;
+    text-align: center;
+    border-width: 2px;
+    border-style: solid;
+    color: #ffffff;
+    border-color: #ffffff;
+    background-color:#31D7FE;
+    border-radius: 100px;
+
+}
+.year-img{
+  margin-top: 15px;
+  margin-left: 60px;
+  width: 60px;
+  height: 60px;
 }
 </style>
