a134694f8032a6ccb3f755ab3c3935a7d9b1560c
unknown <weihao@zlgmcu.com>
2018-06-11 11:08:54 +0800

    修复bug/

--- src/Login.vue
+++ src/Login.vue
@@ -82,7 +82,7 @@ export default {
         console.log("will jupm");
         navigator.push(
           {
-            url: "http://192.168.0.101:8081/BottomTab.html",
+            url: "http://192.168.12.75:8081/BottomTab.html",
             animated: "true"
           },
           event => {
