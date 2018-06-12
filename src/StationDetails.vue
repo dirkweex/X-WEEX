0f3c2573feb188525033b540880088ed43e605ce
unknown <weihao@zlgmcu.com>
2018-06-12 10:09:22 +0800

    整合代码。

--- src/StationDetails.vue
+++ src/StationDetails.vue
@@ -20,11 +20,63 @@
             </div>
             <!-- 轮播图悬浮固定显示天气 -->
             <text style="left:20px;top:220px;color:red;position:absolute">天气信息</text>
+
+            <!-- 顶部标签页 -->
+            <div>
+                <wxc-tab-page ref="wxc-tab-page"  :tab-titles="tabTiles" :tab-styles="tabStyles" title-type="icon" :tab-page-height="tabPageHeight" @wxcTabPageCurrentTabSelected="wxcTabPageCurrentTabSelected">
+                    <list v-for="(v,index) in tabList" :key="index" class="item-container" :style="{height:(tabPageHeight-tabStyles.height)+'px'}">
+                        <cell v-for="(demo,key) in v" class="cell" :key="key">
+                            <wxc-pan-item :ext-id="'1-'+(v)+'-'+(key)"
+                                            @wxcPanItemPan="wxcPanItemPan"
+                                            >
+                                <div class="content">
+                                    <text>{{key}}</text>
+                                </div>
+                            </wxc-pan-item>
+                        </cell>
+                    </list>
+                </wxc-tab-page>
+            </div>
         </div>
     </div>
+ 
 </template>
 <script>
+const dom=weex.requireModule('dom');
+import {WxcTabPage,WxcPanItem,Utils,BindEnv} from 'weex-ui';
+// import Config from "./bottomtabconfig.js";
+import bottomtabconfig from './bottomtabconfig.js';
 export default {
+    components:{WxcTabPage,WxcPanItem},
+    data:()=>({
+        tabTitles:bottomtabconfig.tabTitles,
+        tabStyles:bottomtabconfig.tabStyles,
+        tabList:[],
+        demoList:[1,2,3,4],
+        tabPageHeight:1334
+    }),
+    created(){
+        this.tabPageHeight=Utils.env.getPageHeight();
+        this.tabList=[...Array(this.tabTitles.length).keys()].map(i=>[]);
+        Vue.set(this.tabList,0,this.demoList);
+    },
+    methods:{
+        wxcTabPageCurrentTabSelected(e){
+            const self =this;
+            const index =e.page;
+            if(!Utils.isNonEmptyArray(self.tabList[index])){
+                setTimeout(()=>{
+                    Vue.set(self.tabList,index,self.demoList);
+                },100);
+            }
+
+        },
+        wxcPanItemPan(e){
+            if(BindEnv.supportsEBForAndroid()){
+                this.$refs['wxc-tab-page'].bindExp(e.element);
+            }
+        }
+    },
     data() {
         return {
             imageList: [
@@ -42,15 +94,37 @@ export default {
     }
 };
 </script>
-
-    
+ 
 <style scoped>
+.item-container {
+    widows: 750px;
+    background-color: #f2f3f4;
+}
+.border-cell {
+    background: #f2f3f4;
+    width: 750px;
+    height: 24px;
+    align-items: center;
+    justify-content: center;
+    border-bottom-width: 1px;
+    border-style: solid;
+    border-color: #e0e0e0;
+}
+.cell {
+    border-color: #ffffff;
+}
+.content {
+    widows: 750px;
+    height: 300px;
+    border-bottom-width: 1px;
+    align-items: center;
+    justify-content: center;
+}
 .image {
-    width: 600px;
+    width: 800px;
     height: 300px;
 }
 .slider {
-    width: 700px;
     height: 300px;
     border-width: 2px;
     justify-content: flex-end;
@@ -75,9 +149,9 @@ export default {
 .indicator {
     padding: 20px;
     /* height: 300px;  */
-    item-color:gteen;
-    item-selected-color:red;
-    item-size:15px;
+    item-color: gteen;
+    item-selected-color: red;
+    item-size: 20px;
     /* position: absolute; */
     /* top: 200px;
     left: 200px; */
